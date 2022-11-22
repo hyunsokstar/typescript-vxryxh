8831fba5c195061ae3ec4e3a5d489f5e2fb70f77
hyunsokstar <32297817+hyunsokstar@users.noreply.github.com>
2022-11-22 18:09:17 +0900

    Initial commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+
+const dept_code = 'd7';
+const new_dept_code = Number(dept_code.substring(1, 2));
+
+const range = [...new Array(new_dept_code)].map((_, i) => i + 1);
+
+console.log(new_dept_code);
+console.log(range);
+
+appDiv.innerHTML = `<h1>dept_code = ${new_dept_code} 계층 정보 ${range}</h1>`;
